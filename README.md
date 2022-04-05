884c9ba241e83397be6e12979908aca7a31b6962
muharib0410 <abagithub0410@gmail.com>
2022-04-05 12:02:50 +0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NoteTakingApp_CW
+
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mongoose = require("mongoose");
+const port = 3333;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/noteDb");
+
+const Note = mongoose.model("Note", {
+  date: String,
+  authour: String,
+  category: String,
+  body: String,
+});
+
+app.get("/", (req, res) => {
+  Note.find({}, (err, note) => {
+    res.render("index", { memo : note });
+  });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create");
+});
+
+app.post("/create_note", (req, res) => {
+  console.log(req.body);
+  const note = new Note(req.body);
+  note.save();
+
+  Note.find({}, (err, note) => {
+    if (!err) {
+      console.log(note);
+      //   res.render("index", { note: note });
+      res.redirect("/");
+    } else {
+      console.log("error is ", err);
+    }
+  });
+});
+
+app.get("/delete/:id", (req, res) => {
+  Note.findByIdAndDelete(req.params.id, (err) => {
+    err ? console.error(err) : console.log("deleted successfully");
+
+    res.redirect("/");
+  });
+});
+
+app.get("/edit/:id", (req, res) => {
+  Note.find({ _id: req.params.id }, (err, note) => {
+    if (!err) {
+      res.render("update", { note: note });
+    } else {
+      console.table(err);
+    }
+  });
+});
+
+app.post("/edit/:id", (req, res) => {
+  Note.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: req.body },
+    (err, result) => {
+      if (!err) {
+        res.redirect("/");
+        console.log("edited successfully !");
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log("listening on port 3333");
+});
